6a5239c31f40199d0d96b7dc05bc1a3b9c954bc5
veankatanathan <veankatanathan@gmail.com>
2021-08-03 13:30:33 +0530

    password Function

---
+++ src/org/password/Password.java
@@ -0,0 +1,11 @@
+package org.password;
+
+public class Password {
+	public static void pswrd() {
+		System.out.println("password is");
+	}
+	public static void main(String[] args) {
+		pswrd();
+	}
+
+}
